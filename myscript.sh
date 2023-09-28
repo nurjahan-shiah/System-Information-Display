a71f974a13baa6283756e58e54076fd035d375d8
Nurjahan Shiah <70992112+nurjahan-shiah@users.noreply.github.com>
2023-09-28 00:42:51 -0400

    System Information Display

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,13 @@
 
+//current date and time
+echo "Current Date and Time: $(date)"
+
+//Displaying CPU usage using the top command
+echo "CPU Usage:"
+top -n 1 | grep "Cpu(s)" | awk '{print $2+$4}'
+
+//Displaying available disk space using the df command
+echo "Available Disk Space:"
+df -h
+
+
